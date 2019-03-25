e14308da30feada898afac61890efdb2a580250f
Kristina Valentinyova <kristinavalentinyova@gmail.com>
2019-03-25 15:13:19 +0100

    connnect

--- bazarproject/connect.php
+++ bazarproject/connect.php
@@ -5,6 +5,17 @@ $username = "root";
 $password = "";
 $dbname = "test";
 
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
+    
+    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 

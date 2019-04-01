c3842155c6439cac1842cd786a0a873f9f22b25a
Kristina Valentinyova <kristinavalentinyova@gmail.com>
2019-04-01 13:41:22 +0200

    connect=prepisanie mien

--- bazarproject/connect.php
+++ bazarproject/connect.php
@@ -10,10 +10,10 @@ foreach ($_SERVER as $key => $value) {
         continue;
     }
     
-    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+    $servername = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $username = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $password = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
 
 // Create connection
